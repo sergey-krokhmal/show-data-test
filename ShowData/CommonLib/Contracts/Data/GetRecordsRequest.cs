640bf73f4f64136cffce8ee7ecb508977317b622
sergey-krokhmal <xai.sergey.krokhmal@gmail.com>
2018-04-17 22:35:15 +0300

    Add names and orders

--- ShowData/CommonLib/Contracts/Data/GetRecordsRequest.cs
+++ ShowData/CommonLib/Contracts/Data/GetRecordsRequest.cs
@@ -12,11 +12,13 @@ namespace CommonLib.Contracts.Data
         /// <summary>
         /// Filter date from
         /// </summary>
+        [DataMember(Name = "from", Order = 1)]
         public DateTime DateFrom { get; set;}
 
         /// <summary>
         /// Filter date to
         /// </summary>
+        [DataMember(Name = "to", Order = 2)]
         public DateTime DateTo { get; set; }
     }
 }
