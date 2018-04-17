25b18864cabcbfa0d8565934875c3ccbb2f5e08c
sergey-krokhmal <xai.sergey.krokhmal@gmail.com>
2018-04-17 21:26:36 +0300

    Create base WCF data service

---
+++ ShowData/CommonLib/Contracts/Service/IDataService.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace CommonLib.Contracts.Service
+{
+    [ServiceContract]
+    public interface IDataService
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: Добавьте здесь операции служб
+    }
+
+
+    // Используйте контракт данных, как показано в примере ниже, чтобы добавить составные типы к операциям служб.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ ShowData/DataService/DataService.svc.cs
@@ -0,0 +1,26 @@
+﻿using CommonLib.Contracts.Service;
+using System;
+
+namespace DataService
+{
+    public class DataService : IDataService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
