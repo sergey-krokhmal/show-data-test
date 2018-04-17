9f2c0e9a2ac4ea0e1f8654138a051eb414b6a330
sergey-krokhmal <xai.sergey.krokhmal@gmail.com>
2018-04-17 21:49:56 +0300

    Add DataRecord DataContract

---
+++ ShowData/CommonLib/Contracts/Data/DataRecord.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLib.Contracts.Data
+{
+    /// <summary>
+    /// Data contract represents some data record
+    /// </summary>
+    [DataContract]
+    public class DataRecord
+    {
+        /// <summary>
+        /// Record's id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Record's date
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Record's text
+        /// </summary>
+        public string Text { get; set; }
+    }
+}
