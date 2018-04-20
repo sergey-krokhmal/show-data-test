6d241ee35b935b69f8725754254d72921ebbd7c7
sergey-krokhmal <sergey-krokhmal@users.noreply.github.com>
2018-04-20 10:01:48 +0300

    Table apply and show

--- ShowData/DataFront/Scripts/Site.js
+++ ShowData/DataFront/Scripts/Site.js
@@ -1,6 +1,14 @@
 ﻿$(document).ready(()=>{
     loadFromCoockie();
+
+    $('.error-modal').on('hidden.bs.modal', function (e) {
+        $('.error-modal .error-code').html('');
+        $('.error-modal .error-message').html('');
+    });
 });
+
+var mainDateTimeFormat = "DD.MM.YYYY HH:mm:ss";
+var mainDateFormat = "DD.MM.YYYY";
 
 function showData()
 {
@@ -8,11 +16,11 @@ function showData()
     var font = $('#fontSelect1').val();
     
     var dateFrom = $('#dateFromPicker1').val();
-    var momentDateFrom = moment(dateFrom + ' 00:00:00', "DD.MM.YYYY HH:mm:ss");
+    var momentDateFrom = moment(dateFrom + ' 00:00:00', mainDateTimeFormat);
     momentDateFrom.utc();
 
     var dateTo = $('#dateToPicker1').val();
-    var momentDateTo = moment(dateTo + ' 23:59:59', "DD.MM.YYYY HH:mm:ss");
+    var momentDateTo = moment(dateTo + ' 23:59:59', mainDateTimeFormat);
     momentDateTo.utc();
 
     saveToCoockies(color, font, dateFrom, dateTo);
@@ -20,8 +28,29 @@ function showData()
     var query = $('.get-data-form').serialize();
     $.ajax({
         url: 'http://localhost:25136/DataService.svc/GetDataRecords',
-        data: { "from": "15.04.2018 00:00:00", "to":"20.04.2018 00:00:00"},
-        success: function (data) { console.log(data)},
+        data: { "from": momentDateFrom.format(mainDateTimeFormat), "to": momentDateTo.format(mainDateTimeFormat)},
+        success: function (data) {
+            $('.table-panel').css('color', color);
+            $('.table-panel').css('font-family', font);
+            if (data !== undefined) {
+                if (data.sc == true && data.val != null) {
+                    $('.table-container').html('');
+                    if (data.val.length > 0) {
+                        var table = makeHtmlDataTable(data.val);
+                        $('.table-container').html(table);
+                        $('.no-data-mark').hide();
+                        $('.table-panel').removeClass('hide');
+                    } else {
+                        $('.no-data-mark').show();
+                    }
+                    $('table-panel').show();
+                } else {
+                    $('.error-modal .error-code').html(data.cd);
+                    $('.error-modal .error-message').html(data.er);
+                    $('.error-modal').modal();
+                }
+            }
+        },
         dataType: 'json'
     });
 }
@@ -59,4 +88,27 @@ function loadFromCoockie() {
     if (dateTo !== undefined) {
         $('#dateToPicker1').val(dateTo);
     }
+}
+
+function makeHtmlDataTable(dataArray) {
+    var table = '<div class="table-responsive"><table class="table">';
+    table += '<tr>' +
+            '<th class="id">Id</th>' +
+            '<th class="date">Date</th>' +
+            '<th class="text">Text</th>' +
+        '</tr>';
+    for (i = 0; i < dataArray.length; i++) {
+        var dtMoment = moment(dataArray[i].dt, mainDateTimeFormat);
+        dtMoment.local();
+        var dtFormated = dtMoment.format(mainDateFormat);
+        var row =
+            '<tr>' +
+                '<td class="id">' + dataArray[i].id + '</td>' +
+            '<td class="date">' + dtFormated + '</td>' +
+                '<td class="text">' + dataArray[i].txt + '</td>' +
+            '</tr>';
+        table += row;
+    }
+    table += '</table></div>';
+    return table;
 }
